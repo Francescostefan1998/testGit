30f899dcace3cf9653362d84b4f86bc9d76d7f83
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-06-07 10:54:03 +0200

    modifed 3

--- src/it/prova/Gitp.java
+++ src/it/prova/Gitp.java
@@ -5,6 +5,8 @@ public class Gitp {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("end of the program");
+		System.out.println("modified");
+		System.out.println("execution of the program");
 	}
 
 }
